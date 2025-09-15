86a42a5070ae9be893b62894cea163b27ba22cdc
Rohan Gupta <39908472+DemonDaddy22@users.noreply.github.com>
2025-09-15 20:16:08 +0530

    Setup react directory using create next app

---
+++ react/src/app/page.tsx
@@ -0,0 +1,11 @@
+import styles from '@/styles/page.module.css';
+
+const Home = () => {
+  return (
+    <main>
+      <h1 className={styles.heading}>Ultimate JS Problem Bank</h1>
+    </main>
+  );
+};
+
+export default Home;
